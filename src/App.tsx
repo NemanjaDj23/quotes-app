867574c45ed24e8e38c9925e0555ae8606eb0017
Nemanja Djurovic <ekonemanja@gmail.com>
2023-10-01 12:27:40 +0200

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import Login from './pages/Login/Login';
+import { Route, Routes } from 'react-router-dom';
+import { getTokenFromLocalStorage } from './helpers/tokenHelpers';
 
 function App() {
-  return <Login />;
+  const isLoggedIn = !!getTokenFromLocalStorage();
+  return (
+    <Routes>
+      {!isLoggedIn && <Route index={true} element={<Login />} />}
+
+      {isLoggedIn && <Route index={true} element={<div>Ulogovan</div>} />}
+    </Routes>
+  );
 }
 
 export default App;
