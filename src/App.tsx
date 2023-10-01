e4956fee06f91e3f29380b844155578c64df0680
Nemanja Djurovic <ekonemanja@gmail.com>
2023-10-01 11:39:45 +0200

    Add Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
+import Login from './pages/Login/Login';
 
 function App() {
-  return <div></div>;
+  return <Login />;
 }
 
 export default App;
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { useForm } from '../../hooks/useForm';
+import { login } from '../../http-services/auth';
+import styles from './Login.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
+enum LoginError {
+  NO_CREDENTIALS = 'Please enter credentials.',
+  WRONG_CREDENTIALS = 'Wrong credentials.',
+}
+
+function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const { values, handleChange } = useForm({ username: '', password: '' });
+
+  const handleLogin = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    if (!values.username || !values.password) {
+      setError(LoginError.NO_CREDENTIALS);
+    } else {
+      const { error } = await login(values.username, values.password);
+      if (error) {
+        setError(LoginError.WRONG_CREDENTIALS);
+      }
+    }
+  };
+
+  return (
+    <div className={styles.loginWrapper}>
+      <form onSubmit={handleLogin}>
+        {error && <div className={styles.alert}>{error}</div>}
+        <div className={styles.formGroup}>
+          <label>Username</label>
+          <input type='text' name='username' value={values.username} onChange={handleChange} />
+        </div>
+        <div className={styles.formGroup}>
+          <label>Password</label>
+          <div className={styles.passwordInput}>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              name='password'
+              value={values.password}
+              onChange={handleChange}
+            />
+            {showPassword ? (
+              <FontAwesomeIcon
+                icon={faEyeSlash}
+                className={styles.passwordIcon}
+                onClick={() => setShowPassword(false)}
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faEye}
+                className={styles.passwordIcon}
+                onClick={() => setShowPassword(true)}
+              />
+            )}
+          </div>
+        </div>
+        <button type='submit'>Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
